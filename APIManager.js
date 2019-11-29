3211628d5fb07bb68ab45c769f057a40272f2d07
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2019-11-29 20:47:34 +0200

    Connected the first API

--- APIManager.js
+++ APIManager.js
@@ -5,72 +5,37 @@ class APIManager {
         this.data = {}
     }
 
-    loadData() {
+    // loadData() {
         //you should make all your API requests here
         //each request should update the `data` object accordingly
 
         //Random User Generator API
-
-        $ajax({
-            method: "GET",
-            url: 'https://randomuser.me/api/',
-            dataType: 'json'
+        renderUsers() {
+            $.ajax({
+                method: "GET",
+                url: 'https://randomuser.me/api/',
+                dataType: 'json',
             success: function (data) {
+                    
+                    const firstProfile = data.results.map(u => {
+                        return {
+                            picture: u.picture.large,
+                            firstName: u.name.first,
+                            lastName: u.name.last,
+                            city: u.location.city,
+                            state: u.location.state,
+                        }
+                    })
                 
-            }
+                    this.data = firstProfile                    
+                },
             
             error: function (xhr, text, error) {
-                console.log(text)
-            }
+                    console.log(text)
+                }
 
-        })
-
-        //Random Kanye Quote Generator API
-
-        $ajax({
-            method: "GET",
-            url: 'https://jsonplaceholder.typicode.com/users'
-            success: function (data) {
-
-            }
-            
-            error: function (xhr, text, error) {
-                console.log(text)
-            }
-
-        })
-
-        //PokeAPI
-
-        $ajax({
-            method: "GET",
-            url: 'https://jsonplaceholder.typicode.com/users'
-            success: function (data) {
-
-            }
-            
-            error: function (xhr, text, error) {
-                console.log(text)
-            }
-
-        })
-
-        //Bacon Ipsum API
-
-
-        $ajax({
-            method: "GET",
-            url: 'https://jsonplaceholder.typicode.com/users'
-            success: function (data) {
-
-            }
-            
-            error: function (xhr, text, error) {
-                console.log(text)
-            }
-
-        })
-
-
-    }
+            })
+        }
+        
+    // }
 }
--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // Create instances of your classes
 // Create the loadData and renderData functions - these should use the relevant instance
+const test = new APIManager()
+console.log(test.data)
+test.renderUsers()
