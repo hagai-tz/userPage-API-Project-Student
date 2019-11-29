8ebf7656893f8e626538dfb503d55d5446e64b8e
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2019-11-29 18:02:44 +0200

    Added the repository for the project

--- APIManager.js
+++ APIManager.js
@@ -5,8 +5,72 @@ class APIManager {
         this.data = {}
     }
 
-    loadData(){
+    loadData() {
         //you should make all your API requests here
         //each request should update the `data` object accordingly
+
+        //Random User Generator API
+
+        $ajax({
+            method: "GET",
+            url: 'https://randomuser.me/api/',
+            dataType: 'json'
+            success: function (data) {
+                
+            }
+            
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+
+        })
+
+        //Random Kanye Quote Generator API
+
+        $ajax({
+            method: "GET",
+            url: 'https://jsonplaceholder.typicode.com/users'
+            success: function (data) {
+
+            }
+            
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+
+        })
+
+        //PokeAPI
+
+        $ajax({
+            method: "GET",
+            url: 'https://jsonplaceholder.typicode.com/users'
+            success: function (data) {
+
+            }
+            
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+
+        })
+
+        //Bacon Ipsum API
+
+
+        $ajax({
+            method: "GET",
+            url: 'https://jsonplaceholder.typicode.com/users'
+            success: function (data) {
+
+            }
+            
+            error: function (xhr, text, error) {
+                console.log(text)
+            }
+
+        })
+
+
     }
 }
